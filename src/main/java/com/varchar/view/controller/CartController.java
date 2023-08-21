42a3ad1620bff7cbf96261c9779a1aab1cafd7fa
jihyean <d9rmlatte@gmail.com>
2023-08-21 22:26:33 +0900

    장바구니 모듈화

--- src/main/java/com/varchar/view/controller/CartController.java
+++ src/main/java/com/varchar/view/controller/CartController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.varchar.biz.buy.BuyDetailService;
 import com.varchar.biz.buy.BuyDetailVO;
+import com.varchar.biz.common.Product;
 import com.varchar.biz.tea.TeaService;
 import com.varchar.biz.tea.TeaVO;
 
@@ -32,47 +33,12 @@ public class CartController {
 	// --------------------------------- 장바구니 추가 ---------------------------------
 	@RequestMapping(value = "/insertCart.do")
 	public String insertCart(TeaVO teaVO, HttpSession session) {
-
-		ArrayList<TeaVO> cart = (ArrayList<TeaVO>) session.getAttribute("cart");
 		
-		System.out.println("로그 teaNum: [" + teaVO.getTeaNum() + "]");
-		System.out.println("로그 teaCnt: [" + teaVO.getTeaCnt() + "]"); // 구매할 갯수
-		
-		// 처음부터 teaCheckCnt에 구매할 갯수 가져오는거 검토
-		//teaVO.setTeaCheckCnt(teaVO.getTeaCnt());
-		//System.out.println("InsertChckCnt TeaCheckCnt log : " + teaVO.getTeaCheckCnt());
 		int cnt = teaVO.getTeaCnt();
+		teaVO = teaService.selectOne(teaVO);
+		teaVO.setTeaCnt(cnt);
 		
-		teaVO=teaService.selectOne(teaVO);
-		
-		// 수정필요
-		if (teaVO == null) {
-			return "main.do"; // 해당 상품 없을 경우 ---> 일단 임시로 메인으로 보냄
-		}
-
-		boolean flag = false;
-		if (cart != null) { // 카트에 이미 담긴 상품이 있을 경우
-			for (TeaVO t : cart) {
-				if (t.getTeaNum() == teaVO.getTeaNum()) {
-					t.setTeaCnt(t.getTeaCnt() + cnt);
-					System.out.println("로그 !null cart tVO" + teaVO.getImageUrl());
-					System.out.println("로그 !null cart t" + t.getImageUrl());
-					flag = true;
-					break;
-				}
-			}
-		} else { // 카트가 비어있을 경우
-			System.out.println("로그 null cart" + teaVO.getImageUrl());
-			cart = new ArrayList<TeaVO>();
-		}
-
-		if (!flag) {
-			teaVO.setTeaCnt(cnt);
-			cart.add(teaVO);
-		}
-
-		session.setAttribute("cart", cart);
-
+		Product.cartInsert(teaVO, session);
 		return "redirect:cartPage.do";
 	}
 
@@ -80,18 +46,10 @@ public class CartController {
 	@ResponseBody
 	@RequestMapping(value = "/deleteCart.do")
 	public String deleteCartAction(HttpSession session, TeaVO teaVO) {
-
+		
 		ArrayList<TeaVO> cart = (ArrayList<TeaVO>) session.getAttribute("cart");
-
-		int i = 0;
-		for (TeaVO t : cart) {
-			if (t.getTeaNum() == teaVO.getTeaNum()) {
-				cart.remove(i);
-				break;
-			}
-			i++;
-		}
-
+		int i = Product.cartCheck(teaVO, cart);
+		cart.remove(i);
 		session.setAttribute("cart", cart);
 
 		return "redirect:cartPage.do";
@@ -107,16 +65,9 @@ public class CartController {
 		System.out.println("로그 teaNum: [" + teaVO.getTeaNum() + "]");
 		System.out.println("로그 teaCnt: [" + teaVO.getTeaCnt() + "]");
 
-		int i = 0;
-		for (TeaVO t : cart) {
-			if (t.getTeaNum() == teaVO.getTeaNum()) {
-
-				cart.get(i).setTeaCnt(teaVO.getTeaCnt());
-				System.out.println(i + " 재고 변경됨");
-				break;
-			}
-			i++;
-		}
+		
+		int i = Product.cartCheck(teaVO, cart);
+		cart.get(i).setTeaCnt(teaVO.getTeaCnt());
 
 		session.setAttribute("cart", cart);
 		return "redirect:cartPage.do";
@@ -139,11 +90,9 @@ public class CartController {
 	@RequestMapping(value = "/cartRetry.do")
 	public String cartRetry(HttpSession session, TeaVO teaVO, BuyDetailVO buyDetailVO) {
 
-		ArrayList<TeaVO> cart = (ArrayList<TeaVO>) session.getAttribute("cart");
 		List<BuyDetailVO> buyList = new ArrayList<BuyDetailVO>();
 		
 		System.out.println("현재 카트에 아무것도 없음");
-		System.out.println("cart 로그 : " + cart);
 		System.out.println("로그: buyNum ["+buyDetailVO.getBuyNum()+"]");
 
 		buyDetailVO.setBuySearch("다시담기");
@@ -159,36 +108,10 @@ public class CartController {
 
 			tttVO.setTeaCheckCnt(tttVO.getTeaCnt());
 			tttVO.setTeaCnt(buyList.get(i).getBuyCnt());
-
-			//
-			boolean flag = false;
-			if (cart != null) {
-
-				for (TeaVO t : cart) {
-					System.out.println("t: " + t.getTeaNum());
-					if (t.getTeaNum() == tttVO.getTeaNum()) {
-						t.setTeaCnt(t.getTeaCnt() + tttVO.getTeaCnt());
-						System.out.println("로그 !null cart tttVO" + tttVO.getImageUrl());
-						System.out.println("로그 !null cart t" + t.getImageUrl());
-						flag = true;
-						break;
-					}
-				}
-			}
-			else {
-				System.out.println("로그 null cart" + tttVO.getImageUrl());
-				cart = new ArrayList<TeaVO>();
-			}
-
-			if (!flag) {
-				System.out.println("추가추가");
-				cart.add(tttVO);
-			}
-			//
+			
+			Product.cartInsert(tttVO, session);
 
 		}
-
-		session.setAttribute("cart", cart);
 
 		return "redirect:cartPage.do";
 
