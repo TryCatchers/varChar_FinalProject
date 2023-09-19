844cf31666fa88e16bdbc2074599b6d4b2b26d06
jihyean <d9rmlatte@gmail.com>
2023-09-20 00:34:52 +0900

    09-19(화) 16:31 최신화 :image 패키지 추가 및 파일 이동 :(tea 패키지에 있던 image 파일 모두 삭제 필요)

--- src/main/java/com/varchar/biz/image/ImageDAO.java
+++ src/main/java/com/varchar/biz/image/ImageDAO.java
@@ -1,4 +1,4 @@
-package com.varchar.biz.tea;
+package com.varchar.biz.image;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,16 +17,12 @@ public class ImageDAO {
 	private JdbcTemplate jdbcTemplete;
 	
 	static final private String SQL_SELECTALL = "SELECT IMAGE_URL, IMAGE_DIVISION FROM IMAGE WHERE TEA_REVIEW_NUM = ?";
-	
 //	static final private String SQL_SELECTONE = "";
-	
 	static final private String SQL_INSERT = "INSERT INTO IMAGE(IMAGE_NUM, TEA_REVIEW_NUM, IMAGE_URL, IMAGE_DIVISION) "
-											+ "VALUES((SELECT NVL(MAX(IMAGE_NUM), 0) + 1 FROM IMAGE), ?, ?, (SELECT COUNT(*) FROM IMAGE WHERE TEA_REVIEW_NUM = ?))";
-	
+											+ "VALUES((SELECT NVL(MAX(IMAGE_NUM), 0) + 1 FROM IMAGE), ?, ?, SELECT COUNT(*) FROM IMAGE WHERE TEA_REVIEW_NUM = ?)";
 	static final private String SQL_UPDATE = "UPDATE IMAGE SET IMAGE_URL = ? "
 											+ "WHERE IMAGE_NUM = ? AND IMAGE_DIVISION = ?";
-	
-//	static final private String SQL_DELETE = "";
+	static final private String SQL_DELETE = "DELETE FROM IMAGE WHERE TEA_REVIEW_NUM = ?";
 	
 	public List<ImageVO> selectAll(ImageVO imageVO){
 		Object[] args = { imageVO.getTeaReviewNum() };
@@ -39,11 +35,12 @@ public class ImageDAO {
 
 	public boolean insert(ImageVO imageVO) {
 		
-		int result = jdbcTemplete.update(SQL_INSERT, imageVO.getTeaReviewNum(), imageVO.getImageUrl(), imageVO.getTeaReviewNum());
+		int result = jdbcTemplete.update(SQL_INSERT, imageVO.getTeaReviewNum(), imageVO.getImageUrl(), imageVO.getImageDivision());
 		
 		if(result <= 0) {
 			return false;
 		}
+		
 		return true;
 	}
 	
@@ -58,8 +55,15 @@ public class ImageDAO {
 	}
 	
 	public boolean delete(ImageVO imageVO) {
-		return false;
+		
+		int result = jdbcTemplete.update(SQL_DELETE, imageVO.getTeaReviewNum());
+		
+		if(result <= 0) {
+			return false;
+		}
+		return true;
 	}
+
 }
 
 // ---------------------------------------------------------------
